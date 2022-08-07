4e4dfd5694edd74d8c31455adc370ba806a507c4
samuelfer <samuelfesant@gmail.com>
2022-08-07 10:25:29 -0300

    Navega para o dashboard ao efetuar o login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -38,7 +38,7 @@ export class LoginComponent implements OnInit {
         this.authService.successLogin(
           response.headers.get('Authorization').substring(7)
         );
-        this.router.navigate(['']);
+        this.router.navigate(['dashboard']);
       },
       () => {
         this.toastr.error('Usuário e/ou senha inválidos!', 'Login');
