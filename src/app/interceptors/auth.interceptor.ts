4ae7e2a56e75c9d2d280638a1cf94a47da4d3a0d
samuelfer <samuelfesant@gmail.com>
2022-08-09 19:32:27 -0300

    Add item no menu para sair do sistema

--- src/app/interceptors/auth.interceptor.ts
+++ src/app/interceptors/auth.interceptor.ts
@@ -16,7 +16,7 @@ export class AuthInterceptor implements HttpInterceptor {
     next: HttpHandler
   ): Observable<HttpEvent<any>> {
     let token = localStorage.getItem('token');
-    console.log('Sera que tem token no interceptor ' + token);
+
     if (token) {
       const cloneReq = request.clone({
         headers: request.headers.set('Authorization', `Bearer ${token}`),
