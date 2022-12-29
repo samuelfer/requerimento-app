7ad3a1f579e980b9db2675225eede4b52e199e3d
samuelfer <samuelfesant@gmail.com>
2022-12-28 23:39:43 -0300

    Altere cargo para ser um objeto de Cargo

--- src/app/shared/model/pessoa.model.ts
+++ src/app/shared/model/pessoa.model.ts
@@ -1,10 +1,12 @@
+import { Cargo } from './cargo.model';
 import { TipoPessoa } from './tipo-pessoa.model';
 
 export class Pessoa {
   id?: number;
   nome: string;
-  cargo: string;
+  cargo: Cargo;
   tipoPessoa: TipoPessoa;
+  ativo: boolean;
 
   constructor() {}
 }
