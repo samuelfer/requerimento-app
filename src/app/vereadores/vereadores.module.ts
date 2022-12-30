d2edad37981eb89ec629de6a09009405609243fe
samuelfer <samuelfesant@gmail.com>
2022-12-30 12:03:14 -0300

    Add import de list e form de vereadores

--- src/app/vereadores/vereadores.module.ts
+++ src/app/vereadores/vereadores.module.ts
@@ -1,13 +1,14 @@
-import { VereadoresRoutes } from './vereadores.routing';
-import { VereadoresFormComponent } from './vereadores-form/vereadores-form.component';
-import { VereadoresListComponent } from './vereadores-list/vereadores-list.component';
-import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { ButtonModule } from 'primeng/button';
-import { TableModule } from 'primeng/table';
 import { InputTextModule } from 'primeng/inputtext';
 import { PaginatorModule } from 'primeng/paginator';
+import { TableModule } from 'primeng/table';
+
+import { VereadoresFormComponent } from './vereadores-form/vereadores-form.component';
+import { VereadoresListComponent } from './vereadores-list/vereadores-list.component';
+import { VereadoresRoutes } from './vereadores.routing';
 
 @NgModule({
   declarations: [VereadoresListComponent, VereadoresFormComponent],
