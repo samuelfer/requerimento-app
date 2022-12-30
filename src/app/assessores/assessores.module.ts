feb128a88aca0258f94271c24c56e30a4b37dd1b
samuelfer <samuelfesant@gmail.com>
2022-12-30 12:00:37 -0300

    Import módulos

--- src/app/assessores/assessores.module.ts
+++ src/app/assessores/assessores.module.ts
@@ -1,13 +1,14 @@
-import { AssessoresRoutes } from './assessores.routing';
-import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { ButtonModule } from 'primeng/button';
-import { TableModule } from 'primeng/table';
 import { InputTextModule } from 'primeng/inputtext';
 import { PaginatorModule } from 'primeng/paginator';
-import { AssessoresListComponent } from './assessores-list/assessores-list.component';
+import { TableModule } from 'primeng/table';
+
 import { AssessoresFormComponent } from './assessores-form/assessores-form.component';
+import { AssessoresListComponent } from './assessores-list/assessores-list.component';
+import { AssessoresRoutes } from './assessores.routing';
 
 @NgModule({
   declarations: [AssessoresListComponent, AssessoresFormComponent],
