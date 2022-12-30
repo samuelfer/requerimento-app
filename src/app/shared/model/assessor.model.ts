28814cf1a15b6833d136b0f3a79c0c9c5b7df66e
samuelfer <samuelfesant@gmail.com>
2022-12-30 13:30:37 -0300

    Remove atributo cargo em assessor

--- src/app/shared/model/assessor.model.ts
+++ src/app/shared/model/assessor.model.ts
@@ -1,11 +1,9 @@
 import { TipoPessoa } from 'src/app/shared/model/tipo-pessoa.model';
-import { Cargo } from './cargo.model';
 import { Vereador } from './vereador.model';
 
 export class Assessor {
   id?: number;
   nome: string;
-  cargo: Cargo;
   tipoPessoa: TipoPessoa;
   ativo: boolean;
   vereador: Vereador;
