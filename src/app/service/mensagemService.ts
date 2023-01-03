93851cbf344a67c6876ee1dfb6dee8de4e03ff2b
samuelfer <samuelfesant@gmail.com>
2023-01-03 18:25:19 -0300

    Cria class específica para tratamento e exibição das mensagens de validação

---
+++ src/app/service/mensagemService.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
+
+@Injectable()
+export class MensagemService {
+  constructor(private toastr: ToastrService) {}
+
+  mensagemSucesso(detail: any) {
+    this.toastr.success(detail);
+  }
+
+  mensagemError(error: any, detail: any) {
+    console.log('Res ', error);
+    if (error.error.errors) {
+      error.error.errors.forEach((element: { message: string | undefined }) => {
+        this.toastr.error(element.message);
+      });
+    } else if (error.error.message) {
+      this.toastr.error(error.error.message);
+    } else {
+      this.toastr.error(detail);
+    }
+  }
+
+  mensagemAlerta(detail: any) {
+    this.toastr.warning(detail);
+  }
+}
