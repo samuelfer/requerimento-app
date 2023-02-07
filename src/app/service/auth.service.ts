ab7d991c23f4326e3ca2e28b55e8ce7a62984ada
samuelfer <samuelfesant@gmail.com>
2023-02-07 16:25:08 -0300

    feat: add método específico para recuperar o token do local storage

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -1,4 +1,5 @@
-import { Observable } from 'rxjs';
+import { UsuarioAccessToken } from './../shared/model/user-access-token';
+import { Observable, BehaviorSubject } from 'rxjs';
 import { Credenciais } from '../shared/model/credenciais.model';
 import { environment } from '../../environments/environment';
 import { Injectable } from '@angular/core';
@@ -10,10 +11,16 @@ import { JwtHelperService } from '@auth0/angular-jwt';
 })
 export class AuthService {
   private apiUrl = `${environment.URL_API}/login`;
+  private _isLogado$ = new BehaviorSubject<boolean>(false);
+  isLogado$ = this._isLogado$.asObservable();
 
   jwtService: JwtHelperService = new JwtHelperService();
+  user: UsuarioAccessToken;
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) {
+    this._isLogado$.next(!!this.isAuthenticated());
+    this.user = this.getUser(this.token);
+  }
 
   login(credenciais: Credenciais): Observable<any> {
     return this.http.post(`${this.apiUrl}`, credenciais, {
@@ -24,18 +31,28 @@ export class AuthService {
 
   successLogin(token: string): void {
     localStorage.setItem('token', token);
+    const listRoles = ['ADMIN'];
+    localStorage.setItem('rolesUsuario', JSON.stringify(listRoles));
+    this._isLogado$.next(true);
+    this.user = this.getUser(token);
   }
 
   isAuthenticated() {
-    let token = localStorage.getItem('token');
-
-    if (token !== null) {
-      return !this.jwtService.isTokenExpired(token);
+    if (this.token !== null) {
+      return !this.jwtService.isTokenExpired(this.token);
     }
     return false;
   }
 
   logout() {
     localStorage.clear();
+  }
+
+  get token(): any {
+    return localStorage.getItem('token');
+  }
+
+  private getUser(token: string): UsuarioAccessToken {
+    return JSON.parse(atob(token.split('.')[1])) as UsuarioAccessToken;
   }
 }
