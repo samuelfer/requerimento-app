23afaedebfbf06ef579b255a1bfe318ef0173887
samuelfer <samuelfesant@gmail.com>
2023-02-07 16:23:54 -0300

    feat: corrige import

--- src/app/usuarios/usuarios.service.ts
+++ src/app/usuarios/usuarios.service.ts
@@ -1,4 +1,4 @@
-import { Usuario } from './../shared/model/usuario.mode';
+import { Usuario } from '../shared/model/usuario.model';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
