d1aa670c29d19c03cd8273098f2c09fd3649ecf0
samuelfer <samuelfesant@gmail.com>
2023-02-07 16:19:36 -0300

    feat: add auth guard para validar as roles do usuário

---
+++ src/app/auth/has-role.guard.ts
@@ -0,0 +1,42 @@
+import { MensagemService } from './../service/mensagemService';
+import { Injectable } from '@angular/core';
+import {
+  ActivatedRouteSnapshot,
+  CanActivate,
+  Router,
+  RouterStateSnapshot,
+  UrlTree,
+} from '@angular/router';
+import { Observable } from 'rxjs';
+import { AuthService } from '../service/auth.service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class HasRoleGuard implements CanActivate {
+  constructor(
+    private authService: AuthService,
+    private mensagemService: MensagemService,
+    private router: Router
+  ) {}
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ):
+    | Observable<boolean | UrlTree>
+    | Promise<boolean | UrlTree>
+    | boolean
+    | UrlTree {
+    const isAutorizado = this.authService.user.authorities.includes(
+      route.data.roles
+    );
+
+    if (!isAutorizado) {
+      this.router.navigate(['dashboard']);
+      this.mensagemService.mensagemAlerta(
+        'Você não tem permissão para acessar essa funcionalidade!'
+      );
+    }
+    return isAutorizado;
+  }
+}
