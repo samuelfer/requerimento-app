1b29a31422a53ede2bf417e28ea3b789556e2f86
samuelfer <samuelfesant@gmail.com>
2023-02-11 12:33:09 -0300

    feat: add opção para cadastrar o assessor como usuário do sistema

--- src/app/shared/model/assessor.model.ts
+++ src/app/shared/model/assessor.model.ts
@@ -4,9 +4,11 @@ import { Vereador } from './vereador.model';
 export class Assessor {
   id?: number;
   nome: string;
+  email: string;
   tipoPessoa: TipoPessoa;
   ativo: boolean;
   vereador: Vereador;
-
+  usuarioSistema: boolean;
+  senha: string;
   constructor() {}
 }
--- src/app/shared/model/pessoa.model.ts
+++ src/app/shared/model/pessoa.model.ts
@@ -3,8 +3,9 @@ import { TipoPessoa } from './tipo-pessoa.model';
 export class Pessoa {
   id?: number;
   nome: string;
+  email?: string;
   tipoPessoa: TipoPessoa;
   ativo: boolean;
-
+  usuarioSistema: boolean;
   constructor() {}
 }
